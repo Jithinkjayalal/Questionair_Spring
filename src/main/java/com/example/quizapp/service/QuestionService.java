05abe030ff6ad29718f0d44aff23dc201d790935
Jithinkjayalal <jithinkjayalal@gmail.com>
2024-10-31 23:30:02 +0530

    project completed

--- src/main/java/com/example/quizapp/service/QuestionService.java
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -1,12 +1,11 @@
 package com.example.quizapp.service;
 
-import com.example.quizapp.Question;
+import com.example.quizapp.model.Question;
 import com.example.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
